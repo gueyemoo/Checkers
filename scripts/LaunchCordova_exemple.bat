541bd40f7816a3eefbbfa67ed06697ea19e7716e
dorianosaure <contact.dorianosaure@gmail.com>
2021-11-29 10:49:18 +0100

    [ADD] LaunchCordova_exemple.bat script used to launch cordova environment

---
+++ scripts/LaunchCordova_exemple.bat
@@ -0,0 +1,3 @@
+set ANDROID_SDK_ROOT=D:\M2\android_studio\DevWebDir\Sdk 
+set PATH=D:\M2\android_studio\DevWebDir\Sdk\tools;D:\M2\android_studio\DevWebDir\Sdk\platform-tools;D:\M2\android_studio\DevWebDir\Sdk\build-tools;D:\M2\android_studio\DevWebDir\gradle-7.2\bin;D:\M2\android_studio\DevWebDir\android-studio\bin\;%PATH% 
+call conda activate DevWeb 
