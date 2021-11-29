841be98244c2cc9999d2423084694932d828bc9b
dorianosaure <contact.dorianosaure@gmail.com>
2021-11-29 13:32:21 +0100

    [UPDATE] new definition to exportation of getMongoose function in DB.js file

--- mongo/DB.js
+++ mongo/DB.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test');
-const db = mongoose.connection;
+exports.getMongoose = function () {
+    const mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost:27017/test');
+    const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+    db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function() {
-    console.log("Connection Successful!");
-});
+    db.once('open', function () {
+        console.log("Connection Successful!");
+    });
 
-export function getMongoose() {
     return mongoose;
 }
