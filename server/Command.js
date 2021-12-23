2f50359edaf9e413281f74dda9f6ce707c9a97b8
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-23 19:18:41 +0100

    [ADD] Ajout de la classe Command qui permet de gérer les commandes executable sur le serveur

---
+++ server/Command.js
@@ -0,0 +1,18 @@
+class Command {
+
+    constructor(name, command) {
+        this.name = name;
+        let Validator = require('jsonschema').Validator;
+        this.validator = new Validator();
+        this.schema = require('./schema/' + this.name + '.json')
+        this.command = command;
+    }
+
+    validate(object) {
+        return this.validator.validate(object, this.schema).valid;
+    }
+
+    execute(data) {
+        this.command(data);
+    }
+}
