598b76a2831f99000b0337d8d8b4593ba8c267fa
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-27 17:28:35 +0100

    [UPDATE] Ajout d'un attribut static instance et d'une methode static getInstance dans la class CommandDispatcher.js

--- server/commandDispatcher/class/CommandDispatcher.js
+++ server/commandDispatcher/class/CommandDispatcher.js
@@ -1,8 +1,10 @@
 class CommandDispatcher {
 
-    constructor() {
-        this.commands = []
+    static instance = null;
 
+    constructor() {
+        const Command = require("./Command.js");
+        this.commands = []
         this.commands["end_game"] = new Command("end_game")
         this.commands["form_login"] = new Command("form_login")
         this.commands["init_game"] = new Command("init_game")
@@ -14,6 +16,11 @@ class CommandDispatcher {
         if (!this.commands[command]) return;
         if (!this.commands[command].validate(data)) return;
         this.commands[command].execute(data);
+    }
+
+    static getInstance() {
+        if (CommandDispatcher.instance === null) CommandDispatcher.instance = new CommandDispatcher();
+        return CommandDispatcher.instance;
     }
 
 }
