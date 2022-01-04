0ff31c86a7d1c29218061d9f4edc74436b459e40
dorianosaure <contact.dorianosaure@gmail.com>
2022-01-04 14:44:43 +0100

    [ADD] Ajout de la class Game

---
+++ server/GameManager/class/Game.js
@@ -0,0 +1,9 @@
+class Game {
+    constructor(gameId, player1, player2) {
+        this.gameId = gameId;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+}
+
+module.exports = Game;
